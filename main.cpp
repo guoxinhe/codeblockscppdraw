c02097a11985ec9bf4e827c3eead489cd3a543ea
Xinhe Guo <xguo@greenliant.com>
2018-09-14 15:29:39 +0800

    init add files

---
+++ main.cpp
@@ -0,0 +1,354 @@
+#if defined(UNICODE) && !defined(_UNICODE)
+    #define _UNICODE
+#elif defined(_UNICODE) && !defined(UNICODE)
+    #define UNICODE
+#endif
+
+#include <tchar.h>
+#include <windows.h>
+#include <stdio.h>
+#include <math.h>//for sin(float);
+
+
+#define WIN32_LEAN_AND_MEAN
+int mainLive=1;
+
+DWORD WINAPI ThreadFunc(LPVOID n)
+{
+    int nrLoop=0;
+    while(mainLive>0) {
+        Sleep(10*(DWORD)n);//in ms
+        printf("thread[%d] loop %d waiting for next loop\n", (DWORD)n, nrLoop++);
+    }
+    //Sleep((DWORD)n*1000*2);
+    //return (DWORD)n * 10;
+    return 0;
+}
+HANDLE hThrd1;
+HANDLE hThrd2;
+DWORD exitCode1 = 0;
+DWORD exitCode2 = 0;
+DWORD threadId1, threadId2;
+void startThreads(void) {
+    hThrd1 = CreateThread(NULL, 0, ThreadFunc, (LPVOID)1, 0, &threadId1 );
+    if (hThrd1)
+        printf("Thread 1 launched\n");
+
+    hThrd2 = CreateThread(NULL, 0, ThreadFunc, (LPVOID)200, 0, &threadId2 );
+    if(hThrd2)
+        printf("Thread 2 launched\n");
+}
+int isThreadAllDone(void) {
+        GetExitCodeThread(hThrd1, &exitCode1);
+        GetExitCodeThread(hThrd2, &exitCode2);
+        if ( exitCode1 == STILL_ACTIVE )
+            puts("Thread 1 is still running!");
+        if ( exitCode2 == STILL_ACTIVE )
+            puts("Thread 2 is still running!");
+        if ( exitCode1 != STILL_ACTIVE && exitCode2 != STILL_ACTIVE )
+            return 1;
+        return 0;
+}
+void closeThreads(void) {
+    CloseHandle(hThrd1);
+    CloseHandle(hThrd2);
+    printf("Thread 1 returned %d\n", exitCode1);
+    printf("Thread 2 returned %d\n", exitCode2);
+}
+int waitThreadAlldone(void)
+{
+    mainLive=0;
+    //startThreads();
+    // Keep waiting until both calls to GetExitCodeThread succeed AND
+    // neither of them returns STILL_ACTIVE.
+    for (;;)
+    {
+        if ( isThreadAllDone() )
+            break;
+        Sleep(100);
+    }
+    closeThreads();
+    return EXIT_SUCCESS;
+}
+
+
+/*  Declare Windows procedure  */
+LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+
+/*  Make the class name into a global variable  */
+TCHAR szClassName[ ] = _T("CodeBlocksWindowsApp");
+
+int WINAPI WinMain (HINSTANCE hThisInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR lpszArgument,
+                     int nCmdShow)
+{
+    HWND hwnd;               /* This is the handle for our window */
+    MSG messages;            /* Here messages to the application are saved */
+    WNDCLASSEX wincl;        /* Data structure for the windowclass */
+
+    /* The Window structure */
+    wincl.hInstance = hThisInstance;
+    wincl.lpszClassName = szClassName;
+    wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
+    wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
+    wincl.cbSize = sizeof (WNDCLASSEX);
+
+    /* Use default icon and mouse-pointer */
+    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wincl.lpszMenuName = NULL;                 /* No menu */
+    wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
+    wincl.cbWndExtra = 0;                      /* structure or the window instance */
+    /* Use Windows's default colour as the background of the window */
+    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+
+    /* Register the window class, and if it fails quit the program */
+    if (!RegisterClassEx (&wincl))
+        return 0;
+
+    /* The class is registered, let's create the program*/
+    hwnd = CreateWindowEx (
+           0,                   /* Extended possibilites for variation */
+           szClassName,         /* Classname */
+           _T("Code::Blocks Template Windows App"),       /* Title Text */
+           WS_OVERLAPPEDWINDOW, /* default window */
+           CW_USEDEFAULT,       /* Windows decides the position */
+           CW_USEDEFAULT,       /* where the window ends up on the screen */
+           544,                 /* The programs width */
+           375,                 /* and height in pixels */
+           HWND_DESKTOP,        /* The window is a child-window to desktop */
+           NULL,                /* No menu */
+           hThisInstance,       /* Program Instance handler */
+           NULL                 /* No Window Creation data */
+           );
+
+    /* Make the window visible on the screen */
+    ShowWindow (hwnd, nCmdShow);
+    startThreads();
+
+    int nrLoop=0;
+    /* Run the message loop. It will run until GetMessage() returns 0 */
+    while (GetMessage (&messages, NULL, 0, 0))
+    {
+        /* Translate virtual-key messages into character messages */
+        TranslateMessage(&messages);
+        /* Send message to WindowProcedure */
+        DispatchMessage(&messages);
+        printf("Main thread loop %d waiting for next message\n", nrLoop++);
+    }
+    waitThreadAlldone();
+    printf("Press enter to continue\n");
+    fflush(stdout);
+    getchar();
+
+    /* The program return-value is 0 - The value that PostQuitMessage() gave */
+    return messages.wParam;
+}
+#define TWOPI	(2*3.14159)
+#define TWO_PI TWOPI
+int		cxClient, cyClient;//screen size
+static POINT ptBegin;//mouse left down
+static POINT ptEnd;//mouse left up
+static POINT ptMove;//mouse move
+static int guiDirty=0;//need render
+static int msgCount=0;//debug counter
+static HRGN	hRgnClip;
+
+void drawRaginTest(HDC hdc) {
+    //由四个椭圆形成一个区域，然后把这个区域选入设备环境，
+    //接着从窗口的客户区中心发散绘制一系列直线。这些直线仅出现裁剪区内。
+        double fAngle, fRadius;
+		SetViewportOrgEx(hdc, cxClient/2, cyClient/2, NULL);
+		SelectClipRgn(hdc, hRgnClip);
+
+		// hypot 计算直角三角形斜边的长
+		fRadius = _hypot(cxClient/2.0, cyClient/2.0);
+
+		for (fAngle = 0.0; fAngle < TWO_PI; fAngle += TWO_PI/360)
+		{
+			MoveToEx(hdc, 0, 0, NULL);
+			LineTo(hdc, int(fRadius * cos(fAngle) + 0.5), int(-fRadius * sin(fAngle) + 0.5));
+		}
+}
+void createTestRagin() {
+    //InvalidRect函数使显示的矩形区域无效，可以用来擦除客户区的内容, 并产生一个WM_PAINT消息。
+    //若处理区域而不是矩形可用：InvalidateRgn(hwnd, hRgn, bErase); ValidateRgn(hwnd, hRgn);
+
+    //由四个椭圆形成一个区域
+		HCURSOR	hCursor = SetCursor(LoadCursor(NULL, IDC_WAIT));
+		ShowCursor(TRUE);
+
+		if (hRgnClip)
+			DeleteObject(hRgnClip);
+
+        HRGN			hRgnTemp[6];
+
+		hRgnTemp[0] = CreateEllipticRgn(0,			cyClient/3,		cxClient/2,		2*cyClient/3);
+		hRgnTemp[1] = CreateEllipticRgn(cxClient/2,	cyClient/3,		cxClient,		2*cyClient/3);
+		hRgnTemp[2] = CreateEllipticRgn(cxClient/3,	0,				2*cxClient/3,	cyClient/2);
+		hRgnTemp[3] = CreateEllipticRgn(cxClient/3, cyClient/2,		2*cxClient/3,	cyClient);
+		hRgnTemp[4] = CreateRectRgn(0, 0, 1, 1);
+		hRgnTemp[5] = CreateRectRgn(0, 0, 1, 1);
+		hRgnClip = CreateRectRgn(0, 0, 1, 1);
+
+		CombineRgn(hRgnTemp[4], hRgnTemp[0], hRgnTemp[1], RGN_OR);
+		CombineRgn(hRgnTemp[5], hRgnTemp[2], hRgnTemp[3], RGN_OR);
+		CombineRgn(hRgnClip, hRgnTemp[4], hRgnTemp[5], RGN_XOR);
+
+		int i;
+		for (i = 0; i < 6; i++)
+			DeleteObject(hRgnTemp[i]);
+
+		SetCursor(hCursor);
+		ShowCursor(FALSE);
+}
+void drawMiscShape(HDC hdc) {
+		Rectangle(hdc, cxClient/8, cyClient/8, 7*cxClient/8, 7*cyClient/8);
+		MoveToEx(hdc, 0, 0, NULL);
+		LineTo(hdc, cxClient, cyClient);
+		MoveToEx(hdc, 0, cyClient, NULL);
+		LineTo(hdc, cxClient, 0);
+		Ellipse(hdc, cxClient/8, cyClient/8, 7*cxClient/8, 7*cyClient/8);
+		RoundRect(hdc, cxClient/4, cyClient/4, 3*cxClient/4, 3*cyClient/4, cxClient/4, cyClient/4);
+}
+
+void drawSineWave(HDC hdc) {
+        #define NUM		1000
+        POINT			apt[NUM];
+        int i;
+
+		for (i = 0; i < NUM; i++)
+		{
+			apt[i].x = i*cxClient/NUM;
+			apt[i].y = (int)(cyClient / 2 * (1-sin(TWOPI * i / NUM)));
+		}
+		Polyline(hdc, apt, NUM);
+}
+void RandomFillRect(HDC hdc)
+{
+	HBRUSH		hBrush;
+	RECT		rect;
+
+	if (cxClient == 0 || cyClient == 0)
+		return;
+
+	SetRect(&rect, rand() % cxClient, rand() % cyClient, rand() % cxClient, rand() % cyClient);
+ 	hBrush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
+ 	FillRect(hdc, &rect, hBrush);
+	DeleteObject(hBrush);
+}
+
+
+void onDraw(HWND hwnd)
+{
+    //document: https://blog.csdn.net/wowocpp/article/details/79299016
+    HDC         hdc;
+    PAINTSTRUCT ps;
+    //在处理WM_PAINT消息时使用BeginPaint函数和EndPaint函数：
+    hdc = BeginPaint(hwnd, &ps);                         // 1
+    drawMiscShape(hdc);
+    drawSineWave(hdc);
+    if(guiDirty>0)
+        TextOut(hdc, 0, 0, _T("Dirty World!"), _tcslen(_T("Dirty World!")));
+    else
+        TextOut(hdc, 0, 0, _T("Hello World!"), _tcslen(_T("Hello World!")));
+    Rectangle(hdc, 50, 50, 200, 200);                    // 2
+    Rectangle(hdc, 300, 50, 500, 200);                   // 3
+    Ellipse(hdc, 50, 50, 200, 200);                      // 4
+    Ellipse(hdc, 300, 200, 500, 50);                     // 5
+    EndPaint(hwnd, &ps);                                 // 6
+}
+enum DrawType_e
+    {
+        TYPE_LINE,
+        TYPE_RECT,
+        TYPE_ELLIPSE
+    };
+void checkDirty(HWND hwnd)
+{
+    if(guiDirty>0) {
+        //force update GUI.
+        HDC         hdc;
+        //在处理非WM_PAINT消息时由Windows程序获取：
+        hdc = GetDC(hwnd);
+        //获得用于整个窗口的，而不仅仅是窗口客户区的设备环境句柄：
+        //hdc = GetWindowDC(hwnd);
+        //获取当前整个屏幕的设备环境句柄：
+        //hdc = CreateDC(TEXT("DISPLAY"), NULL, NULL, NULL);
+
+        RandomFillRect(hdc);
+        drawRaginTest(hdc);
+        int nTypy = TYPE_LINE;
+        switch (nTypy)
+        {
+        case TYPE_LINE:
+            MoveToEx(hdc, ptBegin.x, ptBegin.y, NULL);
+            LineTo(hdc, ptEnd.x, ptEnd.y);
+            break;
+        case TYPE_RECT:
+            Rectangle(hdc, ptBegin.x, ptBegin.y, ptEnd.x, ptEnd.y);
+            break;
+        case TYPE_ELLIPSE:
+            Ellipse(hdc, ptBegin.x, ptBegin.y, ptEnd.x, ptEnd.y);
+            break;
+        default:
+            break;
+        }
+        ReleaseDC(hwnd, hdc);
+    }
+}
+
+/*  This function is called by the Windows function DispatchMessage()  */
+
+LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    guiDirty=0;
+    msgCount++;
+    switch (message)                  /* handle the messages */
+    {
+
+        case WM_SIZE://窗口大小消息
+            cxClient = LOWORD(lParam);
+            cyClient = HIWORD(lParam);
+            createTestRagin();
+            return 0;
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            break;
+        case WM_LBUTTONDOWN:
+            ptBegin.x = LOWORD(lParam);
+            ptBegin.y = HIWORD(lParam);
+            guiDirty++;
+            break;
+        case WM_LBUTTONUP:
+            ptEnd.x = LOWORD(lParam);
+            ptEnd.y = HIWORD(lParam);
+            guiDirty++;
+            break;
+        case WM_MOUSEMOVE:
+            ptMove.x = LOWORD(lParam);
+            ptMove.y = HIWORD(lParam);
+            printf("Here is a Mouse move message 0x%03X %d %d\n",message, ptMove.x, ptMove.y);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+            break;
+        case WM_PAINT:
+            onDraw(hwnd);
+            break;
+        case WM_NCHITTEST: //0x84,132
+        case MF_MENUBARBREAK://0x32, 32
+        case WM_NCMOUSEMOVE://0xA0,160, cursor on border for change size
+        case 0x2A2: //cursor inter/leave window
+            //return DefWindowProc (hwnd, message, wParam, lParam);
+            //break;
+        default:                      /* for messages that we don't deal with */
+            printf("Here is a message 0x%03X ignored\n",message);
+            return DefWindowProc (hwnd, message, wParam, lParam);
+    }
+    checkDirty(hwnd);
+
+    return 0;
+}
---
+++ systime.c
@@ -0,0 +1,236 @@
+/**********************************************************
+ *
+ * Proprietary & confidential, All right reserved, 2013
+ *         Greenliant System, Inc.
+ *
+ *     Module Name : wintime.c
+ *     Description : Get windows's system time
+***********************************************************/
+
+
+/***********************************************************
+ *     Guo Xinhe @2018.5.3
+ *         Description : Wrapper for get system's time
+************************************************************/
+
+
+/****************************************************
+ *  Include section
+ * for GCC windows version, do not include any file!
+ *
+****************************************************/
+
+/***************************************************
+ *  Defines section
+ * Add all #defines here
+ *
+****************************************************/
+#ifndef WINBASEAPI
+#ifdef __W32API_USE_DLLIMPORT__
+#define DECLSPEC_IMPORT __declspec(dllimport)
+#define WINBASEAPI DECLSPEC_IMPORT
+#else
+#define WINBASEAPI
+#endif
+#endif
+#define WINAPI __stdcall
+#ifndef VOID
+#define VOID void
+#endif
+
+#define DAYMILLSECONDS 86400000UL//how many milliseconds of a day.
+
+/***************************************************
+ * Structure and Union section
+ * Add all structures and unions here
+ * here hack the SYSTEMTIME, and make it easy portable
+****************************************************/
+typedef unsigned short WORD;
+typedef struct _SYSTEMTIME {
+	WORD wYear;
+	WORD wMonth;
+	WORD wDayOfWeek;
+	WORD wDay;
+	WORD wHour;
+	WORD wMinute;
+	WORD wSecond;
+	WORD wMilliseconds;
+} SYSTEMTIME,*LPSYSTEMTIME;
+
+/***************************************************
+ *  Variables section
+ * Add all variable declaration here
+ *
+****************************************************/
+static SYSTEMTIME sysNow;
+static unsigned long toDay=0;//the day number from 1970/1/1
+static unsigned long initDay=0;//the initialized day number from 1970/1/1
+static unsigned long deltaMilliSeconds=0;//millisecond from initDay
+static unsigned long todayMilliSeconds=0;//millisecond of current day.
+static unsigned long long systemMilliSeconds=0;//millisecond from 1970/1/1
+
+/***************************************************
+ *  Function Prototype Section
+ * Add prototypes for all functions called by this
+ * module, with the exception of runtime routines.
+ *
+****************************************************/
+
+WINBASEAPI VOID WINAPI GetSystemTime(LPSYSTEMTIME);
+unsigned long HostRandom();
+unsigned long HostGetBootMilliSeconds(void);
+unsigned long long currentTimeMillis();//simulate android's method.
+void dumpSystemTime(void);//debug
+
+/*****************************************************************************
+ *  Function Implementation Section
+ * Add prototypes for all functions called by this
+ * module, with the exception of runtime routines.
+ *
+*****************************************************************************/
+static unsigned long const monthOffsetFlat[]={
+  //1 2  3  4  5   6   7   8   9   10  11  12  1   2
+    0,31,59,90,120,151,181,212,243,273,304,334,365,396};
+static unsigned long const monthOffsetLeap[]={
+  //1 2  3  4  5   6   7   8   9   10  11  12  1   2
+    0,31,60,91,121,152,182,213,244,274,305,335,366,397};
+static char *  const WeekName[]={"Sunday","Monday","Tuesday","Wednesday",
+    "Thursday","Friday","Saturday","Sunday","Monday","Tuesday"};
+static char *  const WeekBrnm[]={"Sun","Mon","Tue","Wed","Thu","Fri","Sat",
+    "Sun","Mon","Tue"};
+
+/**
+ * get the total days number from 2000/01/01.
+ */
+static unsigned long getYearDays(SYSTEMTIME * st) {
+    if(st==(void *)0 || st->wYear<2000)
+        return 0;
+    unsigned long offset;
+    if((st->wYear%4)!=0) // not a leap year
+        offset=monthOffsetFlat[st->wMonth-1]+st->wDay-1;
+    else//leap year
+        offset=monthOffsetLeap[st->wMonth-1]+st->wDay-1;
+    offset+=(st->wYear-2000)*365;
+    offset+=(st->wYear-2000+3)/4;//add /4 leap year's day.
+    offset-=(st->wYear-2001)/100;//remove /100 not leap year's day.
+    offset+=(st->wYear-2001)/400;//add /400 leap year's day.
+    return offset;
+}
+/**
+ * get the milliseconds of that day.
+ */
+static unsigned long getDayMillseconds(SYSTEMTIME * st) {
+    if(st==(void *)0)
+        return 0;
+   unsigned long mills=1000 * (st->wHour*3600 + st->wMinute*60
+            + st->wSecond) + st->wMilliseconds;
+    return mills;
+}
+/**
+ * dump current OS support date time value.
+ */
+static void dumpSystemTimeLow(SYSTEMTIME * st) {
+    int printf(const char *format, ...);//do not include <stdio.h>
+    //year/month/day hour:min:sec:mills 2018/5/3 9:18:35:151, #4 Thursday
+    printf("year/month/day hour:min:sec:mills %d/%d/%d %d:%d:%d:%d, #%d %s\n",
+           st->wYear,st->wMonth,st->wDay,st->wHour,st->wMinute,st->wSecond,
+           st->wMilliseconds,st->wDayOfWeek,WeekName[st->wDayOfWeek&7]);
+    printf("Day info: init=%d, today=%d\n",initDay,toDay);
+    printf("Millisecond info: today=%d, delta to init=%d\n",
+           todayMilliSeconds, deltaMilliSeconds);
+    printf("System Milliseconds: %ull\n",systemMilliSeconds);
+}
+static char toStringBuffer[256];
+static char *toStringSystemTime(SYSTEMTIME * st) {
+    extern int sprintf(char *buf, const char *fmt,...);
+    //year/month/day hour:min:sec:mills 2018/5/3 9:18:35:151, #4 Thursday
+    sprintf(toStringBuffer, "year/month/day hour:min:sec:mills %d/%d/%d %d:%d:%d:%d, #%d %s",
+           st->wYear,st->wMonth,st->wDay,st->wHour,st->wMinute,st->wSecond,
+           st->wMilliseconds,st->wDayOfWeek,WeekName[st->wDayOfWeek&7]);
+    return toStringBuffer;
+}
+static char *toStringDateTime(SYSTEMTIME * st) {
+    extern int sprintf(char *buf, const char *fmt,...);
+    //year/month/day hour:min:sec:mills 2018/5/3 9:18:35:151, #4 Thursday
+    sprintf(toStringBuffer, "%d/%d/%d %d:%d:%d:%d %s",
+           st->wYear,st->wMonth,st->wDay,st->wHour,st->wMinute,st->wSecond,
+           st->wMilliseconds,WeekName[st->wDayOfWeek&7]);
+    return toStringBuffer;
+}
+
+char *msToDate(unsigned long ms) {
+    SYSTEMTIME stObj, *st=&stObj;
+    st->wYear=0;
+    st->wMonth=0;
+    st->wDayOfWeek=0;
+    st->wDay=ms/86400000; ms%=86400000;
+    st->wHour=ms/3600000; ms%=3600000;
+    st->wMinute=ms/60000; ms%=60000;
+    st->wSecond=ms/1000;  ms%=1000;
+    st->wMilliseconds=ms;
+    st->wYear=st->wDay/365;
+    st->wMonth=st->wDay/30;
+    st->wDayOfWeek=st->wDay%7;
+    return toStringDateTime(st);
+}
+/**
+ * Get system hardware time, and update local variables
+ */
+static void updateWallClock(void) {
+    GetSystemTime(&sysNow);
+    //dumpSystemTime(&sysNow);
+    toDay=getYearDays(&sysNow);
+    todayMilliSeconds=getDayMillseconds(&sysNow);
+    if(initDay == 0) {
+        initDay=toDay;
+    }
+    //the past days. 32bit max is 24 days.
+    deltaMilliSeconds = (toDay-initDay)*DAYMILLSECONDS + todayMilliSeconds;
+    systemMilliSeconds = (unsigned long long)toDay*DAYMILLSECONDS +
+        (unsigned long long)todayMilliSeconds;
+}
+/**
+ * this module's debug code.
+ */
+void dumpSystemTime(void) {
+    updateWallClock();
+    dumpSystemTimeLow(&sysNow);
+}
+/**
+ * debug utility
+ */
+char *toStringNow(void) {
+    updateWallClock();
+    return toStringDateTime(&sysNow);
+}
+
+/**
+ * this implement Android System.currentTimeMillis().
+ */
+unsigned long long currentTimeMillis(void) {
+    updateWallClock();
+    return systemMilliSeconds;
+}
+/**
+ * get the million seconds from this application start or system boot
+ * valid when system boot <49.71 days. (49 days, 17 hours, 167 seconds)
+ * overflow when boot > that limit.
+ */
+unsigned long HostGetBootMilliSeconds(void) {
+    updateWallClock();
+    return deltaMilliSeconds;
+}
+/**
+ * some API has no random(), here is an alt implement
+ */
+static unsigned long hostRandomSeed=0;
+unsigned long HostRandom(void) {
+    updateWallClock();
+    if(hostRandomSeed==0) {
+    	//updateWallClock();
+        hostRandomSeed = deltaMilliSeconds *11/17;
+    }
+    hostRandomSeed = (hostRandomSeed + deltaMilliSeconds + 49999) * 19/13;
+    //hostRandomSeed = hostRandomSeed + hostRandomSeed/9;
+    return hostRandomSeed;
+}
